e6fe98b772e8c497baa073e778c66d055d320649
WangJian9522 <wj1216163123@gmail.com>
2025-03-15 09:28:02 +0800

    change provider for fix vercel tailwind bug.

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  plugins: process.env.VITE_POSTCSS_DISABLE ? {} : { tailwindcss: {}, autoprefixer: {} },
-};
-
-
-module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
